8c6ffcd50f10b610d13dd891d1a16677428ddaaa
Puranjit Singh Matharoo <puranjit13@gmail.com>
2023-03-30 18:29:04 -0500

    Add files via upload

---
+++ EfficientNetd4-Realtime/app/src/main/assets/main/java/com/example/realtime/MainActivity.kt
@@ -0,0 +1,175 @@
+ package com.example.realtime
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.FileUtils
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.example.realtime.ml.Efficientd4
+import com.example.realtime.ml.Model
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+
+ class MainActivity : AppCompatActivity() {
+
+     lateinit var imageProcessor: ImageProcessor
+     val paint = Paint()
+     lateinit var labels:List<String>
+     lateinit var imageView: ImageView
+     lateinit var cameraDevice: CameraDevice
+     lateinit var cameraManager: CameraManager
+     lateinit var textureView: TextureView
+     lateinit var handler:Handler
+     lateinit var bitmap: Bitmap
+     lateinit var model:Model
+     lateinit var modela:Efficientd4
+//     var colors = listOf<Int>(
+//         Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
+//         Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.LTGRAY, Color.WHITE)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        get_permission()
+
+        labels = FileUtil.loadLabels(this, "labelmap.txt")
+
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(512, 512, ResizeOp.ResizeMethod.BILINEAR)).build()
+
+        modela = Efficientd4.newInstance(this)
+
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        imageView = findViewById(R.id.imageView)
+
+        textureView = findViewById(R.id.textureView)
+        textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                open_camera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                bitmap = textureView.bitmap!!
+
+
+                var image = TensorImage.fromBitmap(bitmap)
+
+                image = imageProcessor.process(image)
+
+                val outputs = modela.process(image)
+//                val detectionResult = outputs.detectionResultList.get(0)
+
+                val location = outputs.locationAsTensorBuffer.floatArray
+                val category = outputs.categoryAsTensorBuffer.floatArray
+                val score = outputs.scoreAsTensorBuffer.floatArray
+                val numOfDetections = outputs.numberOfDetectionsAsTensorBuffer.floatArray
+
+                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable)
+
+                val h = mutable.height
+                val w = mutable.width
+
+                paint.textSize = h/45f
+                paint.strokeWidth = h/175f
+                var x = 0
+
+                score.forEachIndexed{index, fl ->
+                    x = index
+                    x *=4
+
+                    if(fl > 0.5){
+//                        paint.setColor(colors.get(index))
+                        paint.style = Paint.Style.STROKE
+                        canvas.drawRect(RectF(location.get(x+1)*w, location.get(x)*h, location.get(x+3)*w, location.get(x+2)*h), paint)
+                        paint.style = Paint.Style.FILL
+                        canvas.drawText(labels.get(category.get(index).toInt()) + fl.toString(), location.get(x+1)*w, location.get(x)*h, paint)
+                    }
+                }
+
+                imageView.setImageBitmap(mutable)
+            }
+
+        }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+    }
+
+     override fun onDestroy() {
+         super.onDestroy()
+         modela.close()
+     }
+
+     @SuppressLint("MissingPermission")
+     fun open_camera(){
+         cameraManager.openCamera(cameraManager.cameraIdList[0], object:CameraDevice.StateCallback(){
+             override fun onOpened(p0: CameraDevice) {
+                 cameraDevice = p0
+
+                 var surfaceTexture = textureView.surfaceTexture
+                 var surface = Surface(surfaceTexture)
+
+                 var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                 captureRequest.addTarget(surface)
+                 cameraDevice.createCaptureSession(listOf(surface), object: CameraCaptureSession.StateCallback(){
+                     override fun onConfigured(p0: CameraCaptureSession) {
+                         p0.setRepeatingRequest(captureRequest.build(), null, null)
+                     }
+
+                     override fun onConfigureFailed(p0: CameraCaptureSession) {
+                     }
+                 }, handler)
+             }
+
+             override fun onDisconnected(p0: CameraDevice) {
+             }
+
+             override fun onError(p0: CameraDevice, p1: Int) {
+             }
+         }, handler)
+     }
+
+    fun get_permission(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
+        }
+    }
+
+     override fun onRequestPermissionsResult(
+         requestCode: Int,
+         permissions: Array<out String>,
+         grantResults: IntArray
+     ) {
+         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+         if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+             get_permission()
+         }
+     }
+
+}
